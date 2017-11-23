1177b31be19ac4b278e96d87b281e4df3ba9d58b
hzg123 <571407517@qq.com>
2017-11-23 20:43:52 +0800

    wertyu

---
+++ dimeng-springboot/src/main/java/com/jk/DimengSpringbootApplication.java
@@ -0,0 +1,18 @@
+package com.jk;
+
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimengSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimengSpringbootApplication.class, args);
+
+
+		git config --global--replace-all user.email "571407517@qq.com"
+
+		git config --global--replace-all user.name "hzg123"
+
+	}
+}
